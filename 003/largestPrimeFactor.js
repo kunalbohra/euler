a40557e25d430c83433137d19e1bbf6ed7e5886e
Kunal <kunal.bohra@cancerlinq.org>
2018-12-28 17:46:01 -0800

    Adding README

--- 003/largestPrimeFactor.js
+++ 003/largestPrimeFactor.js
@@ -2,10 +2,11 @@ function largestPrimeFactor(n) {
   let maxPrime = -1;
 
   while (n % 2 == 0) {
+    //if n is even, keep dividing by n
     maxPrime = 2;
     n = n / 2;
   }
-
+  //increment by 2 starting at 3, since we are left with an odd number at this point
   for (let i = 3; i <= Math.sqrt(n); i += 2) {
     while (n % i == 0) {
       maxPrime = i;
---
+++ README.md
@@ -0,0 +1,1 @@
+learnings from project Euler
